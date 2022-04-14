6e9ca97eb321b4e1f0d4627b3b7c07dec3ec099d
fanxinqi <fanxinqi@taou.com>
2022-04-14 11:44:44 +0800

    fix:es5 use

--- packages/bitmap/index.js
+++ packages/bitmap/index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2022-04-14 11:16:40
- * @LastEditTime: 2022-04-14 11:41:15
+ * @LastEditTime: 2022-04-14 11:44:33
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /me/mm-rocket/packages/bitmap/index.ts
  */
 
-module.exports = require("./lib/parser");
+module.exports = require("./lib/bitmap");
